fa23319257f390c39e9c3906412e79283fdaeae4
Daniel Greibe <Danielgreibe@gmail.com>
2019-09-23 15:47:36 +0200

    Removed unused code and updated returnmessage from GET and GET/id post calls

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,14 +1,7 @@
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.ConnectionString;
-import com.mongodb.ServerAddress;
-import com.mongodb.MongoCredential;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 
-import java.util.Arrays;
 
 public class DatabaseConnection
 {
@@ -17,11 +10,9 @@ public class DatabaseConnection
     }
     public static MongoClient connect()
     {
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://testuser:testuser@nitrogensensortest-c94pp.azure.mongodb.net/AgricircleDB");
-
-        MongoClient mongoClient = MongoClients.create(connectionString);
-
-        return mongoClient;
+        ConnectionString connectionString =
+                new ConnectionString("mongodb+srv://testuser:testuser@nitrogensensortest-c94pp.azure.mongodb.net/AgricircleDB");
+        return MongoClients.create(connectionString);
 
     }
 }
--- src/main/java/WeatherstationEndpoint.java
+++ src/main/java/WeatherstationEndpoint.java
@@ -24,18 +24,7 @@ public class WeatherstationEndpoint {
 
         MongoCollection<Document> collection = database.getCollection("Weatherstation1");
 
-        String json = "{\"app_id\":\"mcf88_test_1\",\"dev_id\":\"mcf_lww_1\",\"hardware_serial\":\"70B3D58FF100D6FF\",\"port\":2,\"counter\":124,\"confirmed\":true,\"payload_raw\":\"CyUAABDcdDICCAu4AFAAAP9aAAAAIAD/////////AAYA\",\"payload_fields\":{\"avg_wind_speed\":17.698999999999998,\"bar_trend\":0,\"barometer_data\":29.916,\"data_version\":0,\"davis_type\":16,\"day_et\":0.032,\"day_rain\":0,\"forecast_icons\":6,\"frame_id\":0,\"leaf_wetness\":16777215,\"outside_humidity\":80,\"outside_temperature\":13.444444444444446,\"rain_rate\":0,\"report_id\":37,\"soil_moisture\":-1,\"solar_radiation\":90,\"uplink_id\":11,\"uv\":255,\"wind_direction\":184,\"wind_speed\":12.872},\"metadata\":{\"time\":\"2019-09-23T12:40:58.822403683Z\",\"frequency\":868.5,\"modulation\":\"LORA\",\"data_rate\":\"SF7BW125\",\"coding_rate\":\"4/5\",\"gateways\":[{\"gtw_id\":\"eui-7276ff00390301da\",\"timestamp\":1689752084,\"time\":\"2019-09-23T12:40:58Z\",\"channel\":7,\"rssi\":-84,\"snr\":6.8,\"rf_chain\":1},{\"gtw_id\":\"eui-1234567997654321\",\"timestamp\":1471069155,\"time\":\"\",\"channel\":2,\"rssi\":-91,\"snr\":9.3,\"rf_chain\":0},{\"gtw_id\":\"eui-000080029c10db15\",\"timestamp\":355055196,\"time\":\"2019-09-23T12:40:58.798582Z\",\"channel\":7,\"rssi\":-86,\"snr\":9.5,\"rf_chain\":1,\"latitude\":55.73246,\"longitude\":12.39422,\"altitude\":31}]},\"downlink_url\":\"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/mcf88_test_1/ws1dk?key=ttn-account-v2.0zspI9Ggh4YjdCgMWrljs_acBeSZ08e-T0e1ZYj1K0Y\"}\n";
-        Document doc1 = Document.parse(json);
-
-        Document doc = new Document("name", "MongoDB")
-                .append("type", "database")
-                .append("count", 1)
-                .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
-                .append("info", new Document("x", 203).append("y", 102));
-
         collection.insertOne(Document.parse(message));
-        /*
-         */
         mongoClient.close();
 
         //Returns a succes message
@@ -49,7 +38,7 @@ public class WeatherstationEndpoint {
         /*
         Should return all the values in the Database
          */
-        return "Du prøver at hente al data fra vejrstationen";
+        return "HAVEN'T IMPLEMENTED ACCESS TO DATABASE YET, SHOULD RETURN ALL DATA.";
     }
     @GET
     @Path("/{id}")
@@ -58,6 +47,6 @@ public class WeatherstationEndpoint {
         /*
         Should return the weatherstation data with an ID of {id}
          */
-        return "Du prøver at hente data fra en vejrstationsrapport med ID " + id;
+        return "HAVEN'T IMPLEMENTED ACCESS TO DATABASE YET, SHOULD RETURN DATA WITH THE SPECIFIC ID " + id;
     }
 }
