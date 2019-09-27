75b5c139e25890410f786396ebe3b65a2e027bee
Daniel Greibe <Danielgreibe@gmail.com>
2019-09-27 15:09:26 +0200

    Implemented GetAllFromDatabase() in MongoDBDAO implementing the WeatherstationDAO. Returns all the values in the database in JSON format.

--- src/main/java/MongoDBDAO.java
+++ src/main/java/MongoDBDAO.java
@@ -2,10 +2,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoException;
 import com.mongodb.MongoWriteConcernException;
 import com.mongodb.MongoWriteException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import org.bson.Document;
 
 public class MongoDBDAO implements WeatherstationDAO
@@ -25,7 +22,7 @@ public class MongoDBDAO implements WeatherstationDAO
             collection.insertOne(Document.parse(jsonMessage));
             return "Database updated";
         }
-        catch(MongoException mwe ) {
+        catch(MongoException mwe) {
             //  Block of code to handle errors
             return mwe.getMessage();
         }
@@ -38,6 +35,29 @@ public class MongoDBDAO implements WeatherstationDAO
     @Override
     public String getAllFromDatabase()
     {
-        return "Not yet implemented";
+        ConnectionString connectionString =
+                new ConnectionString("mongodb+srv://testuser:testuser@nitrogensensortest-c94pp.azure.mongodb.net/AgricircleDB");
+        MongoClient mongoClient = MongoClients.create(connectionString);
+
+        MongoDatabase database = mongoClient.getDatabase("AgricircleDB");
+
+        MongoCollection<Document> collection = database.getCollection("Weatherstation1");
+
+        try {
+            String returnString = "";
+            FindIterable<Document> iterable = collection.find();
+            for (Document document : iterable) {
+                returnString = returnString + document.toJson();
+            }
+            return returnString;
+        }
+        catch(MongoException mwe ) {
+            //  Block of code to handle errors
+            return mwe.getMessage();
+        }
+        finally
+        {
+            mongoClient.close();
+        }
     }
 }
